b10b00f9e32774b5002a9090746a528ebb567647
Ruben_Campos <ibiza.reymundis@gmail.com>
2023-09-09 18:48:28 -0700

    Se optimizo el codigo

--- proyecto-beta-master/app.js
+++ proyecto-beta-master/app.js
@@ -44,32 +44,49 @@ let impDTFuv = document.getElementById("maquinaUV-modal");
 let impSublimado = document.getElementById("sublimado-modal");
 let laser = document.getElementById("laser-modal");
 
+let impMachine = document.getElementById("imp-machine");
+let tituloBanner = document.getElementById("titulo-banner");
+
 const URL_BBDD = "./bbdd.json";
 
 let cont = 0;
+let contador = 0;
 
-fetch(URL_BBDD)
-    .then( (response) => response.json())    
+// Esta funcion se encarga de obtener la data del JSON
+async function conexion(url){
+
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+    }
+
+conexion(URL_BBDD)
     .then( (data) =>{
-        let impDTFtextil = data.articulos.maquinas.DTF_TEXTIL;
-        let impDTFuv = data.articulos.maquinas.UV_DTF;
-        let impSublimado = data.articulos.maquinas.IMPRESORA_SUBLIMADO;
-        let laser = data.articulos.maquinas.LASER;
 
-        RecorrerImpresoras(impDTFtextil);
-        RecorrerImpresoras(impDTFuv);
-        RecorrerImpresoras(impSublimado);
-        RecorrerImpresoras(laser);
+        let dataDTFtextil = data.articulos.maquinas.DTF_TEXTIL;
+        let dataDTFuv = data.articulos.maquinas.UV_DTF;
+        let dataSublimado = data.articulos.maquinas.IMPRESORA_SUBLIMADO;
+        let datalaser = data.articulos.maquinas.LASER;
 
-        // if (window.location.href.includes("index.html")) {
-        //     RecorrerMaquinas(data.articulos.maquinas.DTG[0]);
-        //     RecorrerMaquinas(data.articulos.maquinas.LASER[2]);
-        //     RecorrerMaquinas(data.articulos.maquinas.LASER[0]); 
-        //     RecorrerMaquinas(data.articulos.maquinas.UV_DTF[2]);
-        //     RecorrerMaquinas(data.articulos.maquinas.UV_DTF[3])  
-        // }
+        RecorrerImpresoras(dataDTFtextil);
+        RecorrerImpresoras(dataDTFuv);
+        RecorrerImpresoras(dataSublimado);
+        RecorrerImpresoras(datalaser);
 
-    });
+    })
+    .catch( (error)=>{
+        console.error("Ocurrió un error:", error);
+})
+
+
+// if (window.location.href.includes("index.html")) {
+//     RecorrerMaquinas(data.articulos.maquinas.DTG[0]);
+//     RecorrerMaquinas(data.articulos.maquinas.LASER[2]);
+//     RecorrerMaquinas(data.articulos.maquinas.LASER[0]); 
+//     RecorrerMaquinas(data.articulos.maquinas.UV_DTF[2]);
+//     RecorrerMaquinas(data.articulos.maquinas.UV_DTF[3])  
+// }
+
 
 function RecorrerImpresoras(datas){
 
@@ -80,49 +97,82 @@ function RecorrerImpresoras(datas){
 }
 
 function CreateCard(item){
-    let div = document.createElement("div");
-    div.className = "contenedor";
+    // mejora la condicion
+    if ( cont !== 4){
 
-    let content = `
-        <img src="${item.imagen}" alt="">
-        <h2>${item.nombre}</h2>
-    `;
+        let div = document.createElement("div");
+        div.className = "contenedor";
 
-    div.innerHTML= content;
- 
-    switch(cont){
-        case 0:
-            impDTFtextil.appendChild(div);
-        break;
+        let content = `
+            <img src="${item.imagen}" alt="">
+            <h2>${item.nombre}</h2>
+        `;
 
-        case 1:
-            impDTFuv.appendChild(div);
-        break;
+        div.innerHTML= content;
+    
+        switch(cont){
+            case 0:
+                impDTFtextil.appendChild(div);
+            break;
 
-        case 2:
-            impSublimado.appendChild(div);
-        break;
+            case 1:
+                impDTFuv.appendChild(div);
+            break;
 
-        case 3:
-            laser.appendChild(div);
-        break;
+            case 2:
+                impSublimado.appendChild(div);
+            break;
+
+            case 3:
+                laser.appendChild(div);
+            break;
+
+            default:
+                console.log("Este caso no existe", cont);
+        }
+    }else{
+
+        let contenedor = document.createElement("div");
+        contenedor.className = "card-content-machine";
+
+        let imgDIV = document.createElement("div");
+        imgDIV.className = "card-content-img";
+        
+        let contentDIV = document.createElement("div");
+        contentDIV.className = "card-content-text";
+
+        let img = `
+            <img src="${item.imagen}" alt="">
+        `;
+        imgDIV.innerHTML = img;
+
+        let content = `
+            <h2>${item.nombre}</h2>
+            <p>${item.modelo}</p>
+        `;
+        contentDIV.innerHTML = content; // Usa textContent en lugar de innerHTML
+
+        // Agregar los divs de img y content como hijos de contenedor
+        contenedor.appendChild(imgDIV);
+        contenedor.appendChild(contentDIV);
+
+        impMachine.appendChild(contenedor);
     }  
 }
-const menuItems = document.querySelectorAll('.index__contain');
-menuItems.forEach((menuItem) => {
-    const popup = menuItem.querySelector('.card-content-machine');
-    menuItem.addEventListener('mouseenter', () => {
-    popup.style.display = 'block';
-    });
-    menuItem.addEventListener('mouseleave', () => {
-    popup.style.display = 'none';
-    });
-});
+// const menuItems = document.querySelectorAll('.index__contain');
+// menuItems.forEach((menuItem) => {
+//     const popup = menuItem.querySelector('.card-content-machine');
+//     menuItem.addEventListener('mouseenter', () => {
+//     popup.style.display = 'block';
+//     });
+//     menuItem.addEventListener('mouseleave', () => {
+//     popup.style.display = 'none';
+//     });
+// });
 
 /********************************************* */
 
-// let impMachine = document.getElementById("imp-machine");
-// let tituloBanner = document.getElementById("titulo-banner");
+
 
 // function RecorrerMaquinas(data){
 //     if (Array.isArray(data)){
@@ -133,23 +183,27 @@ menuItems.forEach((menuItem) => {
 //         CreateCardMachine(data);
 //     }
 // }
-// function capturarId(event) {
-//     event.preventDefault(); // Previene el comportamiento predeterminado del enlace
 
-//     // Obtener el data-id del enlace
-//     const enlace = event.currentTarget;
-//     const id = enlace.getAttribute("data-id");
-//     console.log(id);
 
-//     if (id !== "nosotros" && id !== "index") {
-//         // Redirigir a la página "maquinas.html" y pasar el ID como parámetro en la URL
-//         window.location.href = `maquinas.html?id=${id}`;
-//     } else if (id === "nosotros") {
-//         window.location.href = "nosotros.html";
-//     } else if (id === "index") {
-//         window.location.href = "index.html";
-//     }
-// }
+
+
+function capturarId(event) {
+    event.preventDefault(); // Previene el comportamiento predeterminado del enlace
+
+    // Obtener el data-id del enlace
+    const enlace = event.currentTarget;
+    const id = enlace.getAttribute("data-id");
+    console.log(id);
+
+    if (id !== "nosotros" && id !== "index") {
+        // Redirigir a la página "maquinas.html" y pasar el ID como parámetro en la URL
+        window.location.href = `maquinas.html?id=${id}`;
+    } else if (id === "nosotros") {
+        window.location.href = "nosotros.html";
+    } else if (id === "index") {
+        window.location.href = "index.html";
+    }
+}
 
 // Agregar un evento click a cada enlace de la barra de navegación
 const enlaces = document.querySelectorAll(".nav-links a");
@@ -157,96 +211,62 @@ enlaces.forEach((enlace) => {
     enlace.addEventListener("click", capturarId);
 });
 
-const enlacesCategoria = document.querySelectorAll(".contenedor-cartas a");
-enlacesCategoria.forEach((enlace) => {
-    enlace.addEventListener("click", capturarId);
-});
+// const enlacesCategoria = document.querySelectorAll(".contenedor-cartas a");
+// enlacesCategoria.forEach((enlace) => {
+//     enlace.addEventListener("click", capturarId);
+// });
 
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
-
 tituloBanner.innerHTML = id;
 
 if(id){
-    
-    fetch(URL_BBDD)
-    .then((response) => response.json())
-    .then((data) => {
-        // Aquí, puedes utilizar el ID para obtener la información específica del JSON
-        const maquina = data.articulos.maquinas[id];
-    
-        RecorrerMaquinas(maquina);
-    
-        // Luego, puedes mostrar la información en la página "maquinas.html"
-    });
+    conexion(URL_BBDD)
+        .then( (data) =>{
+            const maquinas = data.articulos.maquinas[id];
+            RecorrerImpresoras(maquinas);
+        })
+
 }else{
     // Aquí puedes manejar el caso en el que no haya un ID en los parámetros de la URL
     console.error("No se encontró un ID en los parámetros de la URL.");
 }
 
-function CreateCardMachine(item, cardType){
-    let contenedor = document.createElement("div");
-    contenedor.className = "card-content-machine";
-
-    let imgDIV = document.createElement("div");
-    imgDIV.className = "card-content-img";
-    
-    let contentDIV = document.createElement("div");
-    contentDIV.className = "card-content-text";
-
-    let img = `
-        <img src="${item.imagen}" alt="">
-    `;
-    imgDIV.innerHTML = img;
-
-    let content = `
-        <h2>${item.nombre}</h2>
-        <p>${item.modelo}</p>
-    `;
-    contentDIV.innerHTML = content; // Usa textContent en lugar de innerHTML
-
-    // Agregar los divs de img y content como hijos de contenedor
-    contenedor.appendChild(imgDIV);
-    contenedor.appendChild(contentDIV);
-
-    impMachine.appendChild(contenedor);
-}
 
 
+// function limpiarContenedor() {
+//     const impMachine = document.getElementById("imp-machine");
+//     while (impMachine.firstChild) {
+//         impMachine.removeChild(impMachine.firstChild);
+//     }
+// }
+// // Obtén una referencia a la lista de categorías
+// const categoryList = document.querySelector(".container-principal");
 
-function limpiarContenedor() {
-    const impMachine = document.getElementById("imp-machine");
-    while (impMachine.firstChild) {
-        impMachine.removeChild(impMachine.firstChild);
-    }
-}
-// Obtén una referencia a la lista de categorías
-const categoryList = document.querySelector(".container-principal");
-
-// Agrega un evento de clic a la lista de categorías
-categoryList.addEventListener("click", (event) => {
-    // Comprueba si se hizo clic en un elemento <li> (categoría)
-    if (event.target.tagName === "LI") {
-        // Obtén el ID de la categoría seleccionada
-        const categoryId = event.target.id;
-        tituloBanner.innerHTML = categoryId; 
-        loadMachinesByCategory(categoryId);
-    }
-});
+// // Agrega un evento de clic a la lista de categorías
+// categoryList.addEventListener("click", (event) => {
+//     // Comprueba si se hizo clic en un elemento <li> (categoría)
+//     if (event.target.tagName === "LI") {
+//         // Obtén el ID de la categoría seleccionada
+//         const categoryId = event.target.id;
+//         tituloBanner.innerHTML = categoryId; 
+//         loadMachinesByCategory(categoryId);
+//     }
+// });
 /*//*************************************************************************************************************************** */
 
-function loadMachinesByCategory(categoryId) {
-    limpiarContenedor();
-    fetch(URL_BBDD)
-    .then((response) => response.json())
-    .then((data) => {
-        let categoria = data.articulos.maquinas[categoryId];
-        RecorrerMaquinas(categoria);
-    })
-    .catch((error) => {
-            console.error("Error al cargar las máquinas:", error);
-        });
-}
+// function loadMachinesByCategory(categoryId) {
+//     limpiarContenedor();
+//     fetch(URL_BBDD)
+//     .then((response) => response.json())
+//     .then((data) => {
+//         let categoria = data.articulos.maquinas[categoryId];
+//         RecorrerMaquinas(categoria);
+//     })
+//     .catch((error) => {
+//             console.error("Error al cargar las máquinas:", error);
+//         });
+// }
 
 
 const mySwiper = new Swiper('.swiper-container', {
